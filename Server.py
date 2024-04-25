59bed4f2ca3db2eda70654332399e8e19feb67d9
ERAGON9 <liorbarak99@gmail.com>
2024-04-25 17:53:44 +0300

    Added the POST endpoint '/scan'.

--- Server.py
+++ Server.py
@@ -1,19 +1,30 @@
-import socket
+from flask import Flask, request, jsonify
 
-HOST = '0.0.0.0'  # Listen on all available network interfaces
-PORT = 12345  # Choose a suitable port number
+app = Flask(__name__)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
 
-    server_socket.listen()
+@app.route('/scan', methods=['POST'])
+def scan():
+    data = request.get_json()  # Get the JSON data from the request
+    # Check if 'barcode' and 'mode' keys exist in the JSON data
+    if 'barcode' in data and 'mode' in data:
+        barcode = data['barcode']
+        mode = data['mode']
 
-    print('Server listening on {HOST}{PORT}')
-    while True:
-        conn, addr = server_socket.accept()
-        with conn:
-            print('Connected by', addr)
-            data = conn.recv(1024)
-            if not data:
-                break
-            print('Received data', data.decode())
+        # Do something with the barcode and mode here
+        if mode == 'add':
+            print(f'Adding {barcode} to database')
+        else:
+            print(f'Removing {barcode} from database')
+        # Return a JSON response
+        response = {'message': 'Data received successfully'}
+        return jsonify(response), 200
+    else:
+        # If 'barcode' or 'mode' keys are missing, return an error response
+        error_response = {'error': 'Barcode and mode are required fields'}
+        return jsonify(error_response), 400
+
+#app.debug = True
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12345)
