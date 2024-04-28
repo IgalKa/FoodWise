c890dec110349f5f8035f92506deaff7da0806d8
Michael Shuster <michaelsu@mta.ac.il>
2024-04-28 18:48:30 +0300

    order

--- models/Refrigerator.py
+++
@@ -1,4 +0,0 @@
-class Refrigerator:
-    def __init__(self, refrigerator_id):
-        self.refrigeratorId = refrigerator_id
-        self.refrigeratorProducts = None
--- models/User.py
+++
@@ -1,5 +0,0 @@
-class User:
-    def __init__(self, user_name, password, connected_refrigerators):
-        self.user_name = user_name
-        self.password = password
-        self.connected_refrigerators = None
