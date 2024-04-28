baa20614ff50491d6ff54e0e853c42865988055e
Michael Shuster <michaelsu@mta.ac.il>
2024-04-28 19:43:13 +0300

    little changes

--- Server/models/Refrigerator.py
+++ Server/models/Refrigerator.py
@@ -1,4 +1,16 @@
 class Refrigerator:
     def __init__(self, refrigerator_id):
-        self.refrigeratorId = refrigerator_id
-        self.refrigeratorProducts = None
+        self.refrigerator_id = refrigerator_id
+        self.products = []
+
+    def add_item(self, product):
+        self.products.append(product)
+
+    def remove_item(self, barcode):
+        for product in self.products:
+            if product.barcode == barcode:
+                if product.quantity ==1:
+                    self.products.remove(product)
+                else:
+                    product.quantity=product.quantity-1
+                return
