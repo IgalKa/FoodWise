d4d7f851ef4cd5b3798b7ba35cb5c18035a4d2da
Michael Shuster <michaelsu@mta.ac.il>
2024-04-28 20:10:28 +0300

    little changes2

--- Server/models/User.py
+++ Server/models/User.py
@@ -2,4 +2,4 @@ class User:
     def __init__(self, user_name, password, connected_refrigerators):
         self.user_name = user_name
         self.password = password
-        self.connected_refrigerators = None
+        self.connected_refrigerators = []
