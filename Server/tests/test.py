c2c55ab1018400258639eb61316e8e1fbdaff745
MichaelShuster1 <michaelshu87@gmail.com>
2024-05-21 13:04:53 +0300

    fixed bug in tests

--- Server/tests/test.py
+++ Server/tests/test.py
@@ -1,8 +1,12 @@
+import sys
+import os
+
+# Add the root directory of your project to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
-
-from Server.database import Database
 from Server.server import app
-
+from Server.database.Database import Database  # Adjust the import path
 
 @pytest.fixture
 def client():
